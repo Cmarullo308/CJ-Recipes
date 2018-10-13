164a9f7b997a5c4729fa89be63428a21ff9fecde
CJ <Ershkabible94@gmail.com>
2018-10-13 14:26:42 -0400

    Add recipe counter
    Counts the number of recipes added and displays it in the console when the plugin is loaded

--- src/me/cj/main/Main.java
+++ src/me/cj/main/Main.java
@@ -18,7 +18,11 @@ public class Main extends JavaPlugin {
 			Material.GRAY_WOOL, Material.LIGHT_GRAY_WOOL, Material.CYAN_WOOL, Material.PURPLE_WOOL, Material.BLUE_WOOL, Material.BROWN_WOOL, Material.GREEN_WOOL, Material.RED_WOOL,
 			Material.BLACK_WOOL };
 
+	static int numberOfRecipes;
+
 	public void onEnable() {
+		numberOfRecipes = 0;
+
 		// Crafting
 		chainedArmor();
 		glowstoneToDust();
@@ -48,46 +52,63 @@ public class Main extends JavaPlugin {
 		// Furnace
 		goldSwordToGold();
 		ironDoorToIron();
+		
+		getLogger().info(numberOfRecipes + " recipes loaded");
 	}
 
 	public void onDisable() {
 		Bukkit.getServer().clearRecipes();
 	}
 
+	private void addRecipe(ShapedRecipe recipe) {
+		Bukkit.getServer().addRecipe(recipe);
+		numberOfRecipes++;
+	}
+
+	private void addRecipe(ShapelessRecipe recipe) {
+		Bukkit.getServer().addRecipe(recipe);
+		numberOfRecipes++;
+	}
+
+	private void addRecipe(FurnaceRecipe recipe) {
+		Bukkit.getServer().addRecipe(recipe);
+		numberOfRecipes++;
+	}
+
 	private void chainedArmor() {
 		// Helmet
 		ShapedRecipe ChainedHelmet = new ShapedRecipe(new NamespacedKey(this, "ChainedHelmet"), new ItemStack(Material.CHAINMAIL_HELMET));
 		ChainedHelmet.shape(new String[] { "###", "# #", "   " }).setIngredient('#', Material.IRON_BARS);
-		Bukkit.getServer().addRecipe(ChainedHelmet);
+		addRecipe(ChainedHelmet);
 		// Chest
 		ShapedRecipe ChainedChestplate = new ShapedRecipe(new NamespacedKey(this, "ChainedChestPlate"), new ItemStack(Material.CHAINMAIL_CHESTPLATE));
 		ChainedChestplate.shape(new String[] { "# #", "###", "###" }).setIngredient('#', Material.IRON_BARS);
-		Bukkit.getServer().addRecipe(ChainedChestplate);
+		addRecipe(ChainedChestplate);
 		// Leggings
 		ShapedRecipe ChainedLeggings = new ShapedRecipe(new NamespacedKey(this, "ChainedLeggings"), new ItemStack(Material.CHAINMAIL_LEGGINGS));
 		ChainedLeggings.shape(new String[] { "###", "# #", "# #" }).setIngredient('#', Material.IRON_BARS);
-		Bukkit.getServer().addRecipe(ChainedLeggings);
+		addRecipe(ChainedLeggings);
 		// Boots
 		ShapedRecipe ChainedBoots = new ShapedRecipe(new NamespacedKey(this, "ChainedBoots"), new ItemStack(Material.CHAINMAIL_BOOTS));
 		ChainedBoots.shape(new String[] { "   ", "# #", "# #" }).setIngredient('#', Material.IRON_BARS);
-		Bukkit.getServer().addRecipe(ChainedBoots);
+		addRecipe(ChainedBoots);
 	}
 
 	private void glowstoneToDust() {
 		ShapelessRecipe glowstonedust = new ShapelessRecipe(new NamespacedKey(this, "GlowstoneToDust"), new ItemStack(Material.GLOWSTONE_DUST, 4));
 		glowstonedust.addIngredient(Material.GLOWSTONE);
-		Bukkit.getServer().addRecipe(glowstonedust);
+		addRecipe(glowstonedust);
 	}
 
 	private void goldSwordToGold() {
 		FurnaceRecipe GoldSwordToGold = new FurnaceRecipe(new NamespacedKey(this, "GoldSwordToGold"), new ItemStack(Material.GOLD_INGOT, 2), Material.GOLDEN_SWORD, 5, 120);
-		Bukkit.getServer().addRecipe(GoldSwordToGold);
+		addRecipe(GoldSwordToGold);
 	}
 
 	private void goldenApple() {
 		ShapedRecipe GoldenApple = new ShapedRecipe(new NamespacedKey(this, "GoldenApple"), new ItemStack(Material.GOLDEN_APPLE));
 		GoldenApple.shape(new String[] { "###", "#^#", "###" }).setIngredient('#', Material.GOLD_NUGGET).setIngredient('^', Material.APPLE);
-		Bukkit.getServer().addRecipe(GoldenApple);
+		addRecipe(GoldenApple);
 	}
 
 	private void grassBlock() {
@@ -96,27 +117,27 @@ public class Main extends JavaPlugin {
 		for (int shapeNum = 0; shapeNum < shapes.length; shapeNum++) {
 			ShapedRecipe grass = new ShapedRecipe(new NamespacedKey(this, "grass" + shapeNum), new ItemStack(Material.GRASS_BLOCK));
 			grass.shape(shapes[shapeNum]).setIngredient('^', Material.WHEAT_SEEDS).setIngredient('#', Material.DIRT);
-			Bukkit.getServer().addRecipe(grass);
+			addRecipe(grass);
 		}
 	}
 
 	private void ironDoorToIron() {
 		String name = "IronDoorToIron";
 		FurnaceRecipe IronDoorToIron = new FurnaceRecipe(new NamespacedKey(this, name), new ItemStack(Material.IRON_INGOT, 2), Material.IRON_DOOR, 5, 200);
-		Bukkit.getServer().addRecipe(IronDoorToIron);
+		addRecipe(IronDoorToIron);
 	}
 
 	private void itemFrameToLeather() {
 		ShapelessRecipe FrameToLeather = new ShapelessRecipe(new NamespacedKey(this, "FrameToLeather"), new ItemStack(Material.LEATHER));
 		FrameToLeather.addIngredient(Material.ITEM_FRAME);
-		Bukkit.addRecipe(FrameToLeather);
+		addRecipe(FrameToLeather);
 	}
 
 	private void jukeBox() {
 		for (int woodType = 0; woodType < woodPlankTypes.length; woodType++) {
 			ShapedRecipe jukebox = new ShapedRecipe(new NamespacedKey(this, "Jukebox" + woodType), new ItemStack(Material.JUKEBOX));
 			jukebox.shape(new String[] { "# #", "#@#", "###" }).setIngredient('#', woodPlankTypes[woodType]).setIngredient('@', Material.REDSTONE);
-			Bukkit.getServer().addRecipe(jukebox);
+			addRecipe(jukebox);
 		}
 	}
 
@@ -125,68 +146,68 @@ public class Main extends JavaPlugin {
 		String name = "lead";
 		ShapedRecipe lead = new ShapedRecipe(new NamespacedKey(this, name), new ItemStack(Material.LEAD));
 		lead.shape(shape).setIngredient('#', Material.STRING);
-		Bukkit.getServer().addRecipe(lead);
+		addRecipe(lead);
 	}
 
 	private void mossyCobbleToCobble() {
 		ShapelessRecipe msbtsb = new ShapelessRecipe(new NamespacedKey(this, "mossyCobbleToCobble"), new ItemStack(Material.COBBLESTONE));
 		msbtsb.addIngredient(Material.MOSSY_COBBLESTONE);
-		Bukkit.getServer().addRecipe(msbtsb);
+		addRecipe(msbtsb);
 	}
 
 	private void mossyCobbleWallToCobbleWall() {
 		ShapelessRecipe MossyCobbleWallToCobbleWall = new ShapelessRecipe(new NamespacedKey(this, "MossyCobbleWallToCobbleWall"), new ItemStack(Material.COBBLESTONE_WALL));
 		MossyCobbleWallToCobbleWall.addIngredient(Material.MOSSY_COBBLESTONE_WALL);
-		Bukkit.getServer().addRecipe(MossyCobbleWallToCobbleWall);
+		addRecipe(MossyCobbleWallToCobbleWall);
 	}
 
 	private void mossyCobblestone() {
 		ShapedRecipe mossyCobblestone = new ShapedRecipe(new NamespacedKey(this, "cobbleToMossyCobble"), new ItemStack(Material.MOSSY_COBBLESTONE, 5));
 		mossyCobblestone.shape(new String[] { "#^#", "^#^", "#^#" }).setIngredient('#', Material.COBBLESTONE).setIngredient('^', Material.WHEAT_SEEDS);
-		Bukkit.getServer().addRecipe(mossyCobblestone);
+		addRecipe(mossyCobblestone);
 	}
 
 	private void mossyStoneBrickToStoneBrick() {
 		// Mossy Stone Brick to Stone Brick
 		ShapelessRecipe msbtsb = new ShapelessRecipe(new NamespacedKey(this, "mossyStoneBrickToStoneBrick"), new ItemStack(Material.STONE_BRICKS));
 		msbtsb.addIngredient(Material.MOSSY_STONE_BRICKS);
-		Bukkit.getServer().addRecipe(msbtsb);
+		addRecipe(msbtsb);
 	}
 
 	private void nameTag() {
 		ShapelessRecipe nametag = new ShapelessRecipe(new NamespacedKey(this, "Nametag"), new ItemStack(Material.NAME_TAG));
 		nametag.addIngredient(Material.IRON_INGOT).addIngredient(Material.STRING);
-		Bukkit.getServer().addRecipe(nametag);
+		addRecipe(nametag);
 	}
 
 	private void netherwartBlockToNetherwart() {
 		ShapelessRecipe NetherwartBlockToNetherwart = new ShapelessRecipe(new NamespacedKey(this, "NetherwartBlockToNetherwart"), new ItemStack(Material.NETHER_WART, 9));
 		NetherwartBlockToNetherwart.addIngredient(Material.NETHER_WART_BLOCK);
-		Bukkit.getServer().addRecipe(NetherwartBlockToNetherwart);
+		addRecipe(NetherwartBlockToNetherwart);
 	}
 
 	private void polishedAndesiteToAndesite() {
 		ShapelessRecipe PolishedAndesiteToAndesite = new ShapelessRecipe(new NamespacedKey(this, "PolishedAndesiteToAndesite"), new ItemStack(Material.ANDESITE));
 		PolishedAndesiteToAndesite.addIngredient(Material.POLISHED_ANDESITE);
-		Bukkit.getServer().addRecipe(PolishedAndesiteToAndesite);
+		addRecipe(PolishedAndesiteToAndesite);
 	}
 
 	private void polishedDioriteToDiorite() {
 		ShapelessRecipe PolishedDioriteToDiorite = new ShapelessRecipe(new NamespacedKey(this, "PolishedDioriteToDiorite"), new ItemStack(Material.DIORITE));
 		PolishedDioriteToDiorite.addIngredient(Material.DIORITE);
-		Bukkit.getServer().addRecipe(PolishedDioriteToDiorite);
+		addRecipe(PolishedDioriteToDiorite);
 	}
 
 	private void polishedGraniteToGranite() {
 		ShapelessRecipe PolishedGraniteToGranite = new ShapelessRecipe(new NamespacedKey(this, "PolishedGraniteToGranite"), new ItemStack(Material.GRANITE));
 		PolishedGraniteToGranite.addIngredient(Material.POLISHED_GRANITE);
-		Bukkit.getServer().addRecipe(PolishedGraniteToGranite);
+		addRecipe(PolishedGraniteToGranite);
 	}
 
 	private void quartzToNetherQuartz() {
 		ShapelessRecipe QuartsNetherQuartz = new ShapelessRecipe(new NamespacedKey(this, "QuartzToNetherQuartz"), new ItemStack(Material.QUARTZ, 4));
 		QuartsNetherQuartz.addIngredient(Material.QUARTZ_BLOCK);
-		Bukkit.getServer().addRecipe(QuartsNetherQuartz);
+		addRecipe(QuartsNetherQuartz);
 	}
 
 	private void rottenFleshToLeather() {
@@ -196,14 +217,14 @@ public class Main extends JavaPlugin {
 		for (int shape = 0; shape < shapes.length; shape++) {
 			ShapedRecipe leather = new ShapedRecipe(new NamespacedKey(this, name + shape), new ItemStack(Material.LEATHER));
 			leather.shape(shapes[shape]).setIngredient('#', Material.ROTTEN_FLESH);
-			Bukkit.getServer().addRecipe(leather);
+			addRecipe(leather);
 		}
 	}
 
 	private void saddle() {
 		ShapedRecipe saddle = new ShapedRecipe(new NamespacedKey(this, "Saddle"), new ItemStack(Material.SADDLE));
 		saddle.shape(new String[] { "###", "! !", "# #" }).setIngredient('#', Material.LEATHER).setIngredient('!', Material.STRING);
-		Bukkit.getServer().addRecipe(saddle);
+		addRecipe(saddle);
 	}
 
 	private void slabsToPlanks() {
@@ -215,7 +236,7 @@ public class Main extends JavaPlugin {
 			for (int shape = 0; shape < shapes.length; shape++) {
 				ShapedRecipe plank = new ShapedRecipe(new NamespacedKey(this, "Plank" + recipieNum), new ItemStack(woodPlankTypes[woodType]));
 				plank.shape(shapes[shape]).setIngredient('#', woodSlabTypes[woodType]);
-				Bukkit.getServer().addRecipe(plank);
+				addRecipe(plank);
 				recipieNum++;
 			}
 		}
@@ -224,13 +245,13 @@ public class Main extends JavaPlugin {
 	private void slime() {
 		ShapelessRecipe slimeball = new ShapelessRecipe(new NamespacedKey(this, "Slime"), new ItemStack(Material.SLIME_BALL));
 		slimeball.addIngredient(Material.VINE);
-		Bukkit.getServer().addRecipe(slimeball);
+		addRecipe(slimeball);
 	}
 
 	private void spiderweb() {
 		ShapedRecipe SpiderWeb = new ShapedRecipe(new NamespacedKey(this, "spiderweb"), new ItemStack(Material.COBWEB));
 		SpiderWeb.shape(new String[] { "# #", " # ", "# #" }).setIngredient('#', Material.STRING);
-		Bukkit.getServer().addRecipe(SpiderWeb);
+		addRecipe(SpiderWeb);
 	}
 
 	private void stoneBricks() {
@@ -240,7 +261,7 @@ public class Main extends JavaPlugin {
 		for (int brick = 0; brick < bricks.length; brick++) {
 			ShapelessRecipe stoneBrickToCobbleStoneBrick = new ShapelessRecipe(new NamespacedKey(this, "BrickToCobblestone" + brick), new ItemStack(Material.COBBLESTONE));
 			stoneBrickToCobbleStoneBrick.addIngredient(bricks[brick]);
-			Bukkit.getServer().addRecipe(stoneBrickToCobbleStoneBrick);
+			addRecipe(stoneBrickToCobbleStoneBrick);
 		}
 	}
 
@@ -249,13 +270,13 @@ public class Main extends JavaPlugin {
 		for (int woolColor = 0; woolColor < woolColors.length; woolColor++) {
 			ShapelessRecipe string = new ShapelessRecipe(new NamespacedKey(this, "WoolToString" + woolColor), new ItemStack(Material.STRING, 4));
 			string.addIngredient(woolColors[woolColor]);
-			Bukkit.getServer().addRecipe(string);
+			addRecipe(string);
 		}
 	}
-	
+
 	private void zombieHeadToLeather() {
 		ShapelessRecipe zombieHeadToLeather = new ShapelessRecipe(new NamespacedKey(this, "ZombieHeadToLeather"), new ItemStack(Material.LEATHER, 2));
 		zombieHeadToLeather.addIngredient(Material.ZOMBIE_HEAD);
-		Bukkit.getServer().addRecipe(zombieHeadToLeather);
+		addRecipe(zombieHeadToLeather);
 	}
 }
